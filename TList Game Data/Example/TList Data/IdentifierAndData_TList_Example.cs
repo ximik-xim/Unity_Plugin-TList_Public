c8c7f6808bdcccef1bd95f37643368e8ddf487d4
NO-243PCBER03I9\ENP <ximik-xim@mail.ru>
2025-06-14 14:03:12 +0300

    - Добавлены возможности бэкапа и востоновление данных из бэкапа ( теперь не нужно вручную перетаскивать SO ключей) - Исправлены ряд багов - Заменены Bat фаилы

--- TList Game Data/Example/TList Data/IdentifierAndData_TList_Example.cs
+++ TList Game Data/Example/TList Data/IdentifierAndData_TList_Example.cs
@@ -7,9 +7,20 @@ public class IdentifierAndData_TList_Example : AbsIdentifierAndData<IndifNameSO_
  [SerializeField] 
  private TList_ExampleKey _dataKey;
 
-
  public override TList_ExampleKey GetKey()
  {
   return _dataKey;
  }
+ 
+#if UNITY_EDITOR
+ public override string GetJsonSaveData()
+ {
+  return JsonUtility.ToJson(_dataKey);
+ }
+
+ public override void SetJsonData(string json)
+ {
+  _dataKey = JsonUtility.FromJson<TList_ExampleKey>(json);
+ }
+#endif
 }
